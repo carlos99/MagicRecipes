ab8b00d78760f98168bc27166f1fffbea9342f43
carlos99 <carlos.cw2106@gmail.com>
2015-05-08 12:19:41 -0600

    Adding path and require carrierwave to application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'carrierwave'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +22,8 @@ module MagicRecipes
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.autoload_paths += %W(#{config.root}/app/uploaders)
+
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
   end
