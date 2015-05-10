8f49cd0e2a50b95a857a76e9b69bc2e95b28cd47
carlos99 <carlos.cw2106@gmail.com>
2015-05-10 12:05:45 -0600

    Adding like-dislike functionality (whitout a gem). The show and index views for recipes has been modified in order to show recipes likes or dislikes. The migration for the likes tables has been created.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 	def index
-		@recipes = Recipe.all
+		@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
 	end
 
 	def show
@@ -34,6 +34,18 @@ class RecipesController < ApplicationController
 			redirect_to recipe_path(@recipe)
 		else
 			render :edit
+		end
+	end
+
+	def like
+		@recipe = Recipe.find(params[:id])
+		like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+		if like.valid?
+			flash[:success] = "Your Vote was succesful"
+			redirect_to :back
+		else
+			flash[:danger] = 'You can like or dislike a recipe once'
+			redirect_to :back
 		end
 	end
 
